7c27a9ee9cddf58219019e7bb18240963fd0bdb7
Alix Fumoleau <alix.fumoleau@gmail.com>
2015-02-11 03:41:58 +0100

    Update hosts

--- router.js
+++ router.js
@@ -2,8 +2,17 @@ var express = require('express');
 var vhost = require('vhost');
 
 var app = express();
-app.use(vhost('labeli.org',                 require('../website/server.js')));
-app.use(vhost('www.labeli.org',             require('../website/server.js')));
-app.use(vhost('api.labeli.org',             require('../api/api.js')));
-app.use(vhost('hook.labeli.org',			require('./hook.js')));
+
+// Website
+//app.use(vhost('labeli.org',       	require('../website/server.js')));
+app.use(vhost('next.labeli.org',		require('../website/server.js')));
+app.use(vhost('*.website.labeli.org',   require('../website/server.js')));
+
+// Api
+app.use(vhost('api.labeli.org',    		require('../api/api.js')));
+app.use(vhost('*.api.labeli.org',  		require('../api/api.js')));
+
+// Router hook
+app.use(vhost('hook.labeli.org',		require('./hook.js')));
+
 app.listen(80);
