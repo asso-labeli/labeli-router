4f4463c55824df9006d8d9b7d53c3348e9b665e5
Alix Fumoleau <alix.fumoleau@gmail.com>
2015-02-11 03:19:43 +0100

    Remove uneeded line

--- router.js
+++ router.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var vhost = require('vhost');
 
-var web = require('./website/server.js');
-
 var app = express();
 app.use(vhost('labeli.org',                 require('../website/server.js')));
 app.use(vhost('www.labeli.org',             require('../website/server.js')));
