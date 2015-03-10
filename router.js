fbcb447033ed9f6ea2e3fd270e51a595b8bf1abe
Alix Fumoleau <alix.fumoleau@gmail.com>
2015-03-10 09:47:53 +0100

    Update router

--- router.js
+++ router.js
@@ -4,15 +4,47 @@ var vhost = require('vhost');
 var app = express();
 
 // Website
-//app.use(vhost('labeli.org',       	require('../website/server.js')));
-app.use(vhost('next.labeli.org',		require('../website/server.js')));
-app.use(vhost('*.website.labeli.org',   require('../website/server.js')));
+try
+{
+    var website = require('../website/server.js');
+    if(website != null)
+    {
+        app.use(vhost('next.labeli.org',		website));
+        app.use(vhost('*.website.labeli.org',   website));
+    }
+}
+catch(e)
+{
+    console.log("Website couldn't be started");
+    console.log(e);
+}
 
 // Api
-app.use(vhost('api.labeli.org',    		require('../api/api.js')));
-app.use(vhost('*.api.labeli.org',  		require('../api/api.js')));
+try
+{
+    var api = require('../api/api.js');
+    if(api != null)
+    {
+        app.use(vhost('api.labeli.org',		api));
+        app.use(vhost('*.api.labeli.org',   api));
+    }
+}
+catch(e)
+{
+    console.log("Api couldn't be started");
+    console.log(e);
+}
 
-// Router hook
-app.use(vhost('hook.labeli.org',		require('./hook.js')));
+// Router
+try
+{
+    app.use(vhost('hook.labeli.org',		require('./hook.js')));
+    
+    catch(e)
+    {
+        console.log("Router hook couldn't be started");
+        console.log(e);
+    }
+}
 
 app.listen(80);
