641b6937f8b8b54bd654c4ca6ca5345be5ff3f0e
AamuLumi <flokauder@hotmail.com>
2015-06-05 16:16:11 +0200

    Add method getRouteFor() to create default route

--- router.js
+++ router.js
@@ -1,22 +1,29 @@
 var httpProxy = require('http-proxy');
 var http = require('http');
 
-var proxy = httpProxy.createProxy();
+var proxy = httpProxy.createProxyServer({});
 
 var routes = {
-        // Router
-        'hook.labeli.org' : 'http://localhost:9000',
-        // Api
-        'api.labeli.org' : 'http://localhost:9010',
-        'hook.api.labeli.org' : 'http://localhost:9011',
-        // Website
-        'next.labeli.org' : 'http://localhost:9020',
-        'hook.next.labeli.org' : 'http://localhost:9021',
+    // Router
+    'hook.labeli.org' : 'http://localhost:9000',
+    // Api
+    'api.labeli.org' : 'http://localhost:9010',
+    'hook.api.labeli.org' : 'http://localhost:9011',
+    // Website
+    'next.labeli.org' : 'http://localhost:9020',
+    'hook.next.labeli.org' : 'http://localhost:9021',
 };
+
+function getRouteFor(host){
+    if (routes[host] == undefined)
+        return "http://localhost:9020";
+
+    return routes[host];
+}
 
 var router = http.createServer(function(req, res)
 {
-    proxy.web(req, res, {target : routes[req.headers.host]});
+    proxy.web(req, res, {target : getRouteFor(req.headers.host)});
 });
 
 router.listen(80);
